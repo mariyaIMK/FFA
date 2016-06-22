e3f484af7ee3d60b7fc0b939ba73e3bb780c4e30
mariyaIMK <mariya.krasteva@spaceconcordia.ca>
2016-06-22 10:29:11 -0400

    ffa_script_prepfold.py

---
+++ ffa_script_prepfold.py
@@ -0,0 +1,29 @@
+import subprocess 
+import sys
+import numpy as np
+import re
+"""
+This script makes prepfold plots by taking files with candidates lists produced by sifting_ffa.py. 
+
+"""
+cands_list_file = str(sys.argv[1])
+cands_list = open(cands_list_file,'r')
+lines = cands_list.readlines()[1:]
+i=0
+
+for fullline in lines: 
+    if fullline.startswith("p2030"):
+        i+=1
+	continue
+
+lines = lines[:i]
+
+for line in lines:
+    split_line = line.split()
+    beam =(split_line[0])
+    if beam.endswith('.dat'):
+        p = float(split_line[1])/1000
+        bash_line = 'prepfold -nosearch -noxwin -topo -p '+str(p)+' '+ beam
+        subprocess.call(bash_line,shell=True)
+                              
+print "Done"
