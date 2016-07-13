4473f6194e8f230733f90e45a53f8fa5af17de6e
mariyaIMK <mariya.krasteva@spaceconcordia.ca>
2016-07-13 15:19:57 -0400

    Update ffa_injection.py

--- ffa_injection.py
+++ ffa_injection.py
@@ -18,8 +18,7 @@ import re
 dm = 10
 beam = 'p2030.20131003.G54.79-01.58.C.b0.00000.fil'
 period = 10.964532 #with 6 decimal places
-smean_max = 0.30 #maximum flux density at which you want to attempt an injection
-smean_min = 0.015 #minimum flux density at which you want to attempt an injection 
+sm = 0.30 #maximum flux density at which you want to attempt an injection 
 
 ##########################################################################
 
@@ -31,29 +30,29 @@ smean_min = 0.015 #minimum flux density at which you want to attempt an injectio
 
 #Define the other variables accordingly 
 
-sm = #write a function to det'n sm  
-rms = #from first prepdata
 
 basename = beam.split('.fil',1)[0]
-filename = basename + '_dm' + dm +'_p' + period + 'sm'+ sm ###verify the syntax
+filename = basename + '_dm' + str(dm) +'_p%f'  % (period) + '_sm%.3f' % (sm)
 
+#sm will get altered at each injection in step 3   
+#rms is defined in step 1 from prepdata
 
 #========================================================================
 
 #presto bash commands 
 
-bash_line1 = 'prepdata -nobary -dm '+ dm ' -o '+ basename + ' ' + beam
+bash_line0 = 'prepdata -nobary -dm '+ str(dm) + ' -o '+ basename + ' ' + beam + ' > prepdata_output.txt'
+bash_line1 = 'grep "Data standard deviation:  *" prepdata_output.txt > rms_output.txt'
 bash_line2 = 'rfifind -time 2.0 -o '+ basename + ' ' + beam 
 
 
+bash_line3 = 'python injectpsr.py --dm '+ str(dm) + ' -p %f' % (period)+ ' -v "1 200 0.5" -s radiometer -c "smean=%.3f' % (sm) + ',gain=10,tsys=33.876,rms=' +rms+'" -o '+ filename +'.fil' + ' ' + basename + '.fil' 
 
-bash_line3 = 'python injectpsr.py --dm '+ dm + ' -p '+period+ ' -v "1 200 0.5" -s radiometer -c "smean='+ sm + ',gain=10,tsys=33.876,rms=' +rms+'" -o '+ filename + ' '+ filename + '.fil' 
+bash_line4 = 'prepdata -nobary -dm '+ str(dm) +' -o ' + filename + ' ' + filename + '.fil' 
 
-bash_line4 = 'prepdata -nobary -dm '+ dm ' -o '+ filename + ' '+ filename + '.fil' 
+bash_line5 = 'prepfold -nopdsearch -nodmsearch -topo -nosearch -noxwin -p %f'  % (period) + ' -o ' + filename + ' ' + filename + '.fil' 
 
-bash_line5 = 'prepfold -nopdsearch -nodmsearch -topo -nosearch -noxwin -p ' + period + ' -o ' + filename + ' '+ filename + '.fil' 
-
-bash_line6 = 'prepdata -nobary -dm '+ dm ' -mask '+ basename + '_rfifind.mask -o '+ filename + ' '+ filename + '.fil'
+bash_line6 = 'prepdata -nobary -dm '+ str(dm) + ' -mask '+ basename + '_rfifind.mask -o '+ filename + ' '+ filename + '.fil'
 
 bash_line7 = 'python ffa.py ' + filename + '.dat --plot'
 
@@ -62,33 +61,48 @@ bash_line7 = 'python ffa.py ' + filename + '.dat --plot'
 
 
 #step 1: Before any beam injection 
-subprocess.call(bash_line1,shell=True) #take the rms from here
+subprocess.call(bash_line0,shell=True)
+subprocess.call(bash_line1,shell=True)
+
+rms_output = open("rms_output.txt")
+line = rms_output.readlines()[0]
+values = line.split()
+rms = (values[3])
+print "The rms value for DM " + str(dm) + " is: "+ rms    #take rms from the prepdata output ## Data standard deviation:  2481.55
+    
+
 subprocess.call(bash_line2,shell=True)
 
 
 
 #step2: Inject, Prepdata, Prepfold, Prepdata, FFA on the right DM 
-subprocess.call(bash_line3,shell=True)
-subprocess.call(bash_line4,shell=True)
-subprocess.call(bash_line5,shell=True)
-subprocess.call(bash_line6,shell=True)
-subprocess.call(bash_line7,shell=True)
 
+for trial in trials 
+
+    subprocess.call(bash_line3,shell=True)
+    subprocess.call(bash_line4,shell=True)
+    subprocess.call(bash_line5,shell=True)
+    subprocess.call(bash_line6,shell=True)
+    subprocess.call(bash_line7,shell=True)
 
 #step3: Check if the pulsar was detected 
-cands_list_file = filename+'_cands.ffa'
-cands_list = open(cands_list_file,'r')
+    cands_list_file = filename+'_cands.ffa'
+    cands_list = open(cands_list_file,'r')
 
-lines = cands_list.readlines()[1]
-split_line = line.split()
-cands_period = (split_line[1])
+    lines = cands_list.readlines()[1]
+    split_line = line.split()
+    cands_period = (split_line[1])
+
+    if float(format(cands_period,'.2f')) == float(format(period,'.2f')): 
+        print "The pulsar was found as the first candidate at flux density: %f" + % (sm) 
+    else
+        print "The pulsar was NOT found as the first candidate"
+        print "The current flux density is: %f" + % (sm)
+        print ""
+        print "An incrementation of the flux density will follow ..." 
+        #sm = sm + 0.5 #check increments or call sm function 
 
 
-if cands_period = period #figure out how to compare only the first 2 decimal places
-    print "The pulsar was found as the first candidate at flux density: " +sm 
-else
-    print "The pulsar was NOT found as the first candidate" 
-    sm = sm + 0.05 #check increments or call sm function 
 
 
 
